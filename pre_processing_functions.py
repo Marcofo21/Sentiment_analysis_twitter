530680fddf1cb0732f7be9a83e80b43c668fd870
Marco Forti <marco.forti@uni-bonn.de>
2022-12-12 15:46:12 +0100

    Added pre-commit configuration and worked on pre_processing part.

--- pre_processing_functions.py
+++ pre_processing_functions.py
@@ -1,16 +1,40 @@
 import re
+import pandas as pd
+
 
 def find_hashtags(tweet):
     """
-    This function extracts text preceded by hashtags (i.e. hashtags in the social media jargon) from lines of text.
+    This function extracts text preceded by hashtags (i.e. hashtags in the social media jargon)
+    from a string of text.
 
         Args:
-            tweet (object): collection of texts.
+            tweet (string): a string of text.
 
-        Return:
-            out (list): list with all unique hashtags in the set of tweets (or other type of text). 
+        Returns:
+            out (list): list with all unique hashtags in the tweet (or other type of text).
     """
 
-    out = re.findall('(#[A-Za-z]+[A-Za-z0-9-_]+)', tweet)
-    
+    out = re.findall("(#[A-Za-z]+[A-Za-z0-9-_]+)", tweet)
+
+    return out
+
+
+def extract_hashtags(tweet, df):
+    """
+    This function extracts all unique hashtags from a column of strings in a dataframe
+    and returns them in a a DataFrame containing all hashtags.
+
+        Args:
+            tweet (string): name of the column to be searched in the dataframe.
+            df (DataFrame): dataframe containing the texts to be searched.
+
+        Returns:
+            out (list): list with all unique hashtags in the set of texts.
+
+    """
+
+    temp = df.tweet.apply(find_hashtags)
+    hashtag_list = temp.to_list()
+    out = pd.DataFrame([item for sublist in hashtag_list for item in sublist])
+
     return out
