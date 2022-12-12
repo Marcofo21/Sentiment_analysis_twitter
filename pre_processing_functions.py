7edfd0d8e46743a869647ed48cbfc22b12da365c
Marco Forti <marco.forti@uni-bonn.de>
2022-12-12 17:31:42 +0100

    Added plot of the most frequent hashtags.

--- pre_processing_functions.py
+++ pre_processing_functions.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 def find_hashtags(tweet):
+
     """
     This function extracts text preceded by hashtags (i.e. hashtags in the social media jargon)
     from a string of text.
@@ -15,26 +16,5 @@ def find_hashtags(tweet):
     """
 
     out = re.findall("(#[A-Za-z]+[A-Za-z0-9-_]+)", tweet)
-
-    return out
-
-
-def extract_hashtags(tweet, df):
-    """
-    This function extracts all unique hashtags from a column of strings in a dataframe
-    and returns them in a a DataFrame containing all hashtags.
-
-        Args:
-            tweet (string): name of the column to be searched in the dataframe.
-            df (DataFrame): dataframe containing the texts to be searched.
-
-        Returns:
-            out (list): list with all unique hashtags in the set of texts.
-
-    """
-
-    temp = df.tweet.apply(find_hashtags)
-    hashtag_list = temp.to_list()
-    out = pd.DataFrame([item for sublist in hashtag_list for item in sublist])
 
     return out
